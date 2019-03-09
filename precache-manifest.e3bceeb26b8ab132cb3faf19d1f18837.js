33f0bd5b53827bbffe9c28c8febd6aa3bd59b32e
rohaam <roham.ashrafi@gmail.com>
2019-03-09 19:27:08 +0330

    Updates

---
+++ precache-manifest.e3bceeb26b8ab132cb3faf19d1f18837.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "99cc2207884f066adb10",
+    "url": "/maktab-resume/static/css/main.0dd8c8d2.chunk.css"
+  },
+  {
+    "revision": "99cc2207884f066adb10",
+    "url": "/maktab-resume/static/js/main.99cc2207.chunk.js"
+  },
+  {
+    "revision": "a1ecc220a20a4269db63",
+    "url": "/maktab-resume/static/js/runtime~main.a1ecc220.js"
+  },
+  {
+    "revision": "9d235b415b65d4dd8474",
+    "url": "/maktab-resume/static/js/2.9d235b41.chunk.js"
+  },
+  {
+    "revision": "afb14a26c23bac60e1fefed3fda3eaeb",
+    "url": "/maktab-resume/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Maktab_Resume/precache-manifest.1ffd52ca9a5cfd103af0df5f6162c5e2.js"
+  "/maktab-resume/precache-manifest.e3bceeb26b8ab132cb3faf19d1f18837.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Maktab_Resume/index.html", {
+workbox.routing.registerNavigationRoute("/maktab-resume/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });

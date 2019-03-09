f65f232fcb880bf5a7c7e7a55f25a9bc1a049551
rohaam <roham.ashrafi@gmail.com>
2019-03-09 18:54:40 +0330

    Updates

---
+++ precache-manifest.1ffd52ca9a5cfd103af0df5f6162c5e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "81f9e861e356eaec803d",
+    "url": "/Maktab_Resume/static/css/main.0dd8c8d2.chunk.css"
+  },
+  {
+    "revision": "81f9e861e356eaec803d",
+    "url": "/Maktab_Resume/static/js/main.81f9e861.chunk.js"
+  },
+  {
+    "revision": "d3f2bdc370f1cc8f4ff9",
+    "url": "/Maktab_Resume/static/js/runtime~main.d3f2bdc3.js"
+  },
+  {
+    "revision": "9d235b415b65d4dd8474",
+    "url": "/Maktab_Resume/static/js/2.9d235b41.chunk.js"
+  },
+  {
+    "revision": "c446c967c164be90ac4f41ce6f340c8d",
+    "url": "/Maktab_Resume/index.html"
+  }
+];
